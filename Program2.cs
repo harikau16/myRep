7506679765f171647bfe7be41cda8568214e1723
harikau16 <121867893+harikau16@users.noreply.github.com>
2023-01-04 05:31:15 +0000

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("What is your name?: ");
+        String strName = Console.ReadLine();
+        String contact = Console.ReadLine();
+        Console.WriteLine("Welcome to the C# Tutorial! {0},{1}", strName,contact);
+
+        }
+    }
+}
